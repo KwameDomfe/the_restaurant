46dc4abe0707f8b2e2f7dc1aeb12c59340b5f1c4
Kwame Domfe Appau <nkadbasics@gmail.com>
2025-11-23 03:53:21 +0000

    Refactor database configuration in settings

--- backend/therestaurant/settings.py
+++ backend/therestaurant/settings.py
@@ -94,10 +94,7 @@ WSGI_APPLICATION = 'therestaurant.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL'),
-        conn_max_age=600
-    )
+    'default': dj_database_url.parse(config('DATABASE_URL'), conn_max_age=600)
 }
 
 
