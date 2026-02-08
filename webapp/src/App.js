df844770a06fff5d5137f4e392753c461941d991
Kwame Domfe Appau <nkadbasics@gmail.com>
2026-02-08 14:11:12 +0000

    qwert

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -27,7 +27,7 @@ import HomePage from './pages/HomePage.jsx';
 const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-    const API_BASE_URL = "http://localhost:8000/api";
+  const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://localhost:8000/api";
   const [restaurants, setRestaurants] = useState([]);
   const [menuItems, setMenuItems] = useState([]);
   const [loading, setLoading] = useState(false);
